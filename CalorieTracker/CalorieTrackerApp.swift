422133ded48f0b666fe9f9fd227db5ce43a7cbbf
juliuspor <150340157+juliuspor@users.noreply.github.com>
2023-12-22 16:41:55 +0100

    Initial Commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Julius Porbeck on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
